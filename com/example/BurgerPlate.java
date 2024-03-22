574d835f5421797f0a71408c850b1a24390286cd
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2024-03-21 22:13:26 -0400

    Initial commit

---
+++ com/example/BurgerPlate.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Kevin Csiffary
+ * @version 1.0
+ * @since 2024-03-22
+ */
+
+// BurgerPlate class
+public final class BurgerPlate {
+
+    /** Private constructor to prevent instantiation. */
+    private BurgerPlate() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
